4e17ba244cdab1fb5807246dd8c069940e208c93
David Fontenelle <114594834+davidfont724@users.noreply.github.com>
2022-09-29 14:28:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,10 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
 
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
